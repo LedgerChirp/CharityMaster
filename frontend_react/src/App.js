958431b94ced7dd328941215cc47fb7dfa4ce31c
Huzaifa-code <huzaifaqureshi037@gmail.com>
2022-10-23 19:24:06 +0530

    Footer added

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Contact from './components/contact/Contact'
 import HomeHero from "./components/HomeHero";
 import Navbar from "./components/Navbar";
+import Footer from "./components/contact/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <HomeHero />
       <Contact />
+      <Footer/>
     </>
   );
 }
