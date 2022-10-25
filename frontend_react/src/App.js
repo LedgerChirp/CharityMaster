f3293502eeca55ab833523aa5f78e0f636275948
Huzaifa-code <huzaifaqureshi037@gmail.com>
2022-10-25 20:22:55 +0530

    added about us

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,13 +1,14 @@
-import "./App.css";
 import Contact from './components/contact/Contact'
 import HomeHero from "./components/HomeHero";
 import Navbar from "./components/Navbar";
 import Footer from "./components/contact/Footer";
+import About from "./components/About";
 function App() {
   return (
     <>
       <Navbar />
       <HomeHero />
+      <About/>
       <Contact />
       <Footer/>
     </>
